653fd7c36175e062ce6efaafe575eae9166932b8
Trinh Dinh Tien <tientd@Trinhs-MacBook-Pro.local>
2023-06-20 10:54:58 +0700

    updated ui responsive header

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -32,34 +32,37 @@ const MainLayout: React.FC<Props> = (props:Props) => {
 
   return (
     <div className="col-12">
-      <nav className="navbar navbar-expand navbar-dark bg-dark header-menu">
-        <div>
+      <div className="row header-container">
+        <div className='col-md-3 col-lg-3 col-sm-3 col-xs-3 header-brand'>
           <Link to={"/"} className="navbar-brand">
             FUNNY MOVIES
           </Link>
         </div>
-      <div className="navbar-nav ml-auto">
-        {currentUser && (
-          <li className="nav-item">
-            <p className="header-welcome">Welcome {currentUser.username}</p>
-          </li>
-        )}
-        <li className="nav-item">
-          <button type="submit" className="btn btn-primary btn-block button-custom">
-            <Link to={"/share-video"} className="nav-link">
-              Share a movie
-            </Link>
-          </button>
-        </li>
-        <li className="nav-item">
-          <button type="submit" className="btn btn-danger btn-block button-custom">
-            <a href="/signin" className="nav-link" onClick={logOut}>
-              Logout
-            </a>
-          </button>
-        </li>
+        <div className="col-md-9 col-lg-9 col-sm-9 col-xs-9">
+          <div className="row header-right">
+            {currentUser && (
+              <div className="col-md-7 col-lg-8 col-sm-5 col-xs-4 mt-2">
+                <p className="header-welcome">Welcome {currentUser.username}</p>
+              </div>
+            )}
+            <div className="col-md-3 col-lg-2 col-sm-4 col-xs-4 mt-2">
+              <button type="submit" className="btn btn-primary btn-block button-custom">
+                <Link to={"/share-video"} className="nav-link">
+                  Share a movie
+                </Link>
+              </button>
+            </div>
+            <div className="col-md-2 col-lg-2 col-sm-3 col-xs-4 mt-2">
+              <button type="submit" className="btn btn-danger btn-block button-custom">
+                <a href="/signin" className="nav-link" onClick={logOut}>
+                  Logout
+                </a>
+              </button>
+            </div>
+          </div>
+          
+        </div>
       </div>
-      </nav>
       <div className="container">
         {props.children}
       </div>
